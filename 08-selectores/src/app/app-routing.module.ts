11484a2deb0f7e965d59ab9a244b0d21fc725370
Martin Selis <98183961+martinselis1978@users.noreply.github.com>
2024-07-19 19:06:41 -0300

    clase-12

--- 08-selectores/src/app/app-routing.module.ts
+++ 08-selectores/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'selector',
-    // loadChilder:lazyLoad
+    loadChildren: () => import('./countries/countries.module').then(m => m.CountriesModule),
   },
   {
     path: '**',
