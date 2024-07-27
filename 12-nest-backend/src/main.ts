eb17ae97d861ccd436895429524449335325f4cc
Martin Selis <98183961+martinselis1978@users.noreply.github.com>
2024-07-27 18:52:37 -0300

    27-07-24

--- 12-nest-backend/src/main.ts
+++ 12-nest-backend/src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
 
   const PORT = process.env.PORT ?? 3000;
 
-  console.log('App corriendo en puerto: ${PORT}');
+  console.log(`App corriendo en puerto: ${PORT}`);
   
   await app.listen( PORT );
 }
--- 13-authApp/src/environments/environments.ts
+++ 13-authApp/src/environments/environments.ts
@@ -1,4 +1,4 @@
 
 export const environment = {
-  baseUrl: 'http://localhost:3000'
+  baseUrl: 'https://nest-auth-backend-martin.up.railway.app'
 }
